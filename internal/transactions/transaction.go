dbae9f8224c28198131d803be654f342585db08f
MathiasHilgert <matthiashilgertlol@gmail.com>
2025-09-05 16:22:19 -0300

    feat: added basic transaction definition

---
+++ internal/transactions/transaction.go
@@ -0,0 +1,16 @@
+package transactions
+
+import "time"
+
+// Transaction represents a financial transaction made into a bank account.
+type Transaction struct {
+	// ID is the unique identifier for the transaction.
+	ID uint
+
+	// Date is the date when the transaction occurred.
+	Date time.Time
+
+	// Amount is the monetary value of the transaction.
+	// In this case, for demo purposes, we don't care about currency or precision.
+	Amount float64
+}
